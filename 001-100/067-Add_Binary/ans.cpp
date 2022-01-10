f922953984bbd4b905213de95ab13da87b8356fa
ccs96307 <ccs96307@gmail.com>
2022-01-10 11:29:37 +0800

    2022-01-10

---
+++ 001-100/067-Add_Binary/ans.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        // Init
+        string sum;
+        int carry = 0;
+        int ai = a.size()-1;
+        int bi = b.size()-1;
+        int temp = 0;
+        
+        // a + b
+        while (ai >= 0 || bi >= 0) {
+            // Compute temp value
+            temp = carry;
+            if (ai >= 0 && a[ai] == '1') ++temp;
+            if (bi >= 0 && b[bi] == '1') ++temp;
+            
+            // Carry
+            carry = temp / 2;
+            
+            // Add to "sum"
+            temp = temp % 2;
+            if (temp == 1) sum =  "1" + sum;
+            else sum = "0" + sum;            
+            
+            // Step
+            --ai;
+            --bi;
+        }
+        
+        // If carry, we need to add the last carray value
+        if (carry) sum = "1" + sum;
+        
+        // Answer
+        return sum;
+    }
+};
