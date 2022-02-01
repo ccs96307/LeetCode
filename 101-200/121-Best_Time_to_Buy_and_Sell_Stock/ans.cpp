15e9329752d33096b992b498dc5608e8e650555e
ccs96307 <ccs96307@gmail.com>
2022-02-02 00:35:12 +0800

    2022-02-01

--- 101-200/121-Best_Time_to_Buy_and_Sell_Stock/ans.cpp
+++ 101-200/121-Best_Time_to_Buy_and_Sell_Stock/ans.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {        
-        int profit=0;
-        int buy=10001;
-                
-        for (int i=0; i<prices.size(); ++i) {
-            if (prices[i] < buy) buy = prices[i];
-            if (profit < prices[i]-buy) profit = prices[i] - buy;
+    int maxProfit(vector<int>& prices) {
+        // Init
+        int profit = 0;
+        int buy = INT_MAX;
+            
+        // Determine
+        for (int price: prices) {
+            buy = min(buy, price);
+            profit = max(profit, price-buy);
         }
-        
+                    
         return profit;
     }
 };
