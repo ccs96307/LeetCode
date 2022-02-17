c6b8a1af3755d8c6073c12dec3617cd73c215971
ccs96307 <ccs96307@gmail.com>
2022-02-17 13:41:14 +0800

    2022-02-17

---
+++ 001-100/039-Combination_Sum/dfs_optimalize.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void DFS(vector<vector<int>>& results, vector<int>& result, vector<int>& candidates, int target, int start) {
+        if (target == 0) {
+            results.push_back(result);
+            return;
+        }
+                
+        for (int i=start; i<candidates.size(); ++i) {
+            if (candidates[i] <= target) {
+                result.push_back(candidates[i]);
+                DFS(results, result, candidates, target-candidates[i], i);
+                result.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        // Init
+        vector<int> result;
+        vector<vector<int>> results;
+        
+        DFS(results, result, candidates, target, 0);
+        
+        return results;
+    }
+};
+
