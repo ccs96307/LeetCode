5c1f68c22edda4028d3ec70a02b47856dff7ab39
clay <ccs96307@gmail.com>
2022-11-18 10:45:13 +0800

    2022-11-18

---
+++ 201-300/263-Ugly_Number/ans.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        // Base case
+        if (n < 1) return false;
+
+        while (n % 2 == 0) n /= 2;
+        while (n % 3 == 0) n /= 3;
+        while (n % 5 == 0) n /= 5;
+        
+        return n == 1;
+    }
+};
+
