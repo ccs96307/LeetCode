8553d82bbf4d69f431177965779739b3ea2519bf
clay <ccs96307@gmail.com>
2022-12-05 09:32:16 +0800

    2022-12-05

---
+++ 801-900/876-Middle_of_the_Linked_List/brute_force.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* currNode = head;
+        int length = 0;
+        
+        while (currNode != nullptr) {
+            ++length;
+            currNode = currNode->next;
+        }
+        
+        ListNode* returnNode = head;
+        for (int i=0; i<length/2; ++i) {
+            returnNode = returnNode->next;
+        }
+        
+        return returnNode;
+    }
+};
+
