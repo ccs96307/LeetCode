3637fd9b37dcb3178cf2b66be3fc5c3c3dfcf74c
clay <ccs96307@gmail.com>
2023-02-04 11:23:48 +0800

    2023-02-04

--- 501-600/567-Permutation_in_String/ans.cpp
+++ 501-600/567-Permutation_in_String/ans.cpp
@@ -2,30 +2,39 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         // Base case
-        if (s1.size() > s2.size()) return false;
-        
+        if (s1.size() > s2.size()) {
+            return false;
+        }
+
         // Init
-        vector<int> v1(26, 0);
-        vector<int> v2(26, 0);
-        
-        // Prepare v1 and v2
+        vector<int> counter1(26, 0);
+        vector<int> counter2(26, 0);
+
+        // Compare s1 and s2
         for (int i=0; i<s1.size(); ++i) {
-            ++v1[int(s1[i])-97];
-            ++v2[int(s2[i])-97];
+            ++counter1[s1[i]-'a'];
+            ++counter2[s2[i]-'a'];
         }
-        
-        // First match
-        if (v1 == v2) return true;
-        
-        // For-loop match
-        for (int i=0; i<s2.size()-s1.size(); ++i) {
-            --v2[int(s2[i])-97];
-            ++v2[int(s2[i+s1.size()])-97];
-            
-            if (v1 == v2) return true;
+        if (counter1 == counter2) {
+            return true;
         }
-        
-        // If not match case, return false
+
+        // Sliding matching
+        int left = 0;
+        int right = s1.size();
+        while (right < s2.size()) {
+            --counter2[s2[left]-'a'];
+            ++counter2[s2[right]-'a'];
+
+            if (counter1 == counter2) {
+                return true;
+            }
+
+            ++left;
+            ++right;
+        }
+
         return false;
     }
 };
+
