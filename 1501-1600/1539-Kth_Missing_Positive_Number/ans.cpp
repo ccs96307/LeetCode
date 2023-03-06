af139b27527002c03ae165465d5d8e0045a09195
clay <ccs96307@gmail.com>
2023-03-06 08:49:21 +0800

    2023-03-06

--- 1501-1600/1539-Kth_Missing_Positive_Number/ans.cpp
+++ 1501-1600/1539-Kth_Missing_Positive_Number/ans.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
+        // Init
         int missing = 1;
 
+        // Comparing with the `arr` one by one
         for (int i=0; i<arr.size(); ++i) {
+            // If `missing` not in `arr`, that means we can count down `k` value
             while (missing != arr[i]) {
                 --k;
                 if (k == 0) {
@@ -14,6 +17,7 @@ public:
             ++missing;
         }
 
+        // If `arr` is over and we do not find the answer, we can calculate the results directly
         return missing + k - 1;
     }
 };
