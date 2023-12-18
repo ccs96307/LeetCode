9ce78c6c8b548a78f39e666c6217e6bac9734736
clay <ccs96307@gmail.com>
2023-12-18 10:36:49 +0800

    2023-12-18

---
+++ 1901-2000/1913-Maximum_Product_Difference_Between_Two_Pairs/optimal.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) {
+        // Init
+        int max1 = INT_MIN;
+        int max2 = INT_MIN;
+        int min1 = INT_MAX;
+        int min2 = INT_MAX;
+
+        // Traversal
+        for (int& num: nums) {
+            if (num >= max1) {
+                max2 = max1;
+                max1 = num;
+            }
+            else if (num >= max2) {
+                max2 = num;
+            }
+            
+            if (num <= min1) {
+                min2 = min1;
+                min1 = num;
+            }
+            else if (num <= min2) {
+                min2 = num;
+            }
+        }
+
+        return max1 * max2 - min1 * min2;
+    }
+};
+
