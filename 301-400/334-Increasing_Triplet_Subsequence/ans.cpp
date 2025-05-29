cb7466e1aa6bc7e615320a08d6dde2ced7163841
Clay <ccs96307@gmail.com>
2025-05-29 11:57:11 +0800

    2025-05-29

---
+++ 301-400/334-Increasing_Triplet_Subsequence/ans.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int first = INT_MAX;
+        int second = INT_MAX;
+
+        for (int i=0; i<nums.size(); ++i) {
+            if (nums[i] <= first) {
+                first = nums[i];
+            }
+            else if (nums[i] <= second) {
+                second = nums[i];
+            }
+            else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
