6a72c04e7e61c84b6ae063b06d60696ed330891e
Clay <ccs96307@gmail.com>
2025-07-30 00:24:50 +0800

    2025-07-30

--- 001-100/042-Trapping_Rain_Water/two_pointer.cpp
+++ 001-100/042-Trapping_Rain_Water/two_pointer.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        // Init
-        vector<int> vol;
-        int tempHeight = 0;
-        
-        // Left
-        for (int i=0; i<height.size(); ++i) {
-            tempHeight = max(tempHeight, height[i]);
-            vol.push_back(tempHeight-height[i]);
+        int water = 0;
+        int left = 0;
+        int right = height.size() - 1;
+        int leftHeightest = 0;
+        int rightHeightest = 0;
+
+        while (left < right) {
+            if (height[left] >= height[right]) {
+                if (height[right] >= rightHeightest) {
+                    rightHeightest = height[right];
+                }
+                else {
+                    water += rightHeightest - height[right];
+                }
+                --right;
+            }
+            else {
+                if (height[left] >= leftHeightest) {
+                    leftHeightest = height[left];
+                }
+                else {
+                    water += leftHeightest - height[left];
+                }
+                ++left;
+            }
         }
-        
-        // Right
-        tempHeight = 0;
-        for (int i=height.size()-1; i>=0; --i) {
-            tempHeight = max(tempHeight, height[i]);
-            vol[i] = min(vol[i], tempHeight-height[i]);
-        }
-        
-        return accumulate(vol.begin(), vol.end(), 0);
+
+        return water;
     }
 };
-
