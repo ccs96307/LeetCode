7c756c8cc3a70778f624d01b49f81d23b115d89b
Clay <ccs96307@gmail.com>
2025-08-05 11:52:03 +0800

    2025-08-05

---
+++ 2401-2500/2461-Maximum_Sum_of_Distinct_Subarray_With_Length_K/optim.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        long long result = 0;
+        long long temp = 0;
+        int diff = 0;
+        vector<int> counts(100001, 0); 
+
+        for (int i=0; i<k; ++i) {
+            temp += nums[i];
+            ++counts[nums[i]];
+
+            if (counts[nums[i]] == 2) {
+                ++diff;
+            }
+        }
+
+        if (diff == 0) {
+            result = temp;
+        }
+
+        int s = 0;
+        for (int i=k; i<nums.size(); ++i) {
+            temp += nums[i];
+            temp -= nums[s];
+
+            ++counts[nums[i]];
+            if (counts[nums[i]] == 2) {
+                ++diff;
+            }
+
+            --counts[nums[s]];
+            if (counts[nums[s]] == 1) {
+                --diff;
+            }
+
+            if (diff == 0) {
+                result = max(result, temp);
+            }
+
+            ++s;
+        }
+
+        return result;
+    }
+};
+
