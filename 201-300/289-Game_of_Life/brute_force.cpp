9ceb9f60eb9dbf8203b9bd244751442bf3b2d8a2
Clay <ccs96307@gmail.com>
2025-09-25 10:19:22 +0800

    2025-09-25

--- 201-300/289-Game_of_Life/brute_force.cpp
+++ 201-300/289-Game_of_Life/brute_force.cpp
@@ -1,48 +1,48 @@
 class Solution {
 public:
-    int alive(vector<vector<int>>& board, int i, int j) {
+    int countLiveAround(int i, int j, int m, int n, vector<vector<int>>& board) {
+        static vector<pair<int, int>> dirs = {
+            {-1, -1}, {-1, 0}, {-1, 1},
+            { 0, -1},          { 0, 1},
+            { 1, -1}, { 1, 0}, { 1, 1}
+        };
+
         int live = 0;
-        int dead = 0;
-        
-        for (int _i=i-1; _i<=i+1; ++_i) {
-            for (int _j=j-1; _j<=j+1; ++_j) {
-                if (_i < 0 || _j < 0 || _i >= board.size() || _j >= board[0].size()) {
-                    continue;
-                }
-                
-                if (_i == i && _j == j) continue;
-                
-                if (board[_i][_j]) ++live;
-                else ++dead;
+        for (auto [dy, dx]: dirs) {
+            int x = j + dx;
+            int y = i + dy;
             
+            if (x >= 0 && x < n && y >= 0 && y < m && board[y][x] == 1) {
+                ++live;
             }
         }
-        
-        if (board[i][j] == 1) {
-            if (live < 2 || live >= 4) return 0;
-            else return 1;
-        } 
-        else if (live == 3) return 1;
-        
-        return board[i][j];
+
+        return live;
     }
-    
+
     void gameOfLife(vector<vector<int>>& board) {
-        // Init
-        vector<int> changes;
-        
-        // Record
-        for (int i=0; i<board.size(); ++i) {
-            for (int j=0; j<board[0].size(); ++j) {
-                changes.push_back(alive(board, i, j));
+        vector<vector<int>> newBoard = board;
+
+        int m = board.size();
+        int n = board[0].size();
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int live = countLiveAround(i, j, m, n, board);
+
+                if (live < 2 || live > 3) {
+                    newBoard[i][j] = 0;
+                }
+                else if (live == 3 && board[i][j] == 0) {
+                    newBoard[i][j] = 1;
+                }
+                else {
+                    newBoard[i][j] = board[i][j];
+                }
             }
         }
-        
-        // Update
-        int n = board[0].size();
-        for (int i=0; i<changes.size(); ++i) {
-            board[i/n][i%n] = changes[i];
-        }
+
+        board = newBoard;
     }
 };
 
